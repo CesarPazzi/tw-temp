97a12d8ae449daef4b1b13bc7fff7192c90192eb
Cesar Pazzi <csrpazzi@gmail.com>
2020-11-25 09:59:50 -0600

    translating comments to english and some removed

--- main.py
+++ main.py
@@ -1,45 +1,28 @@
-# importamos Beautiful Soup, urllib y Twython
 import bs4 as bs
 import urllib.request
 from twython import Twython
 import datetime
 import os
 
-######### GLOBALES ######### 
+######### GLOBALS ######### 
 
 yahoo_url = os.environ['YAHOO_URL']
-
-# definimos la URL que queremos Scrapear con la variable 'url'
 url = urllib.request.urlopen(yahoo_url).read()
+page = bs.BeautifulSoup(url, "html5lib")
+hour = datetime.datetime.now()
+local_hour = int(hour.hour)-6 #Need to fix this
 
-# Se crea un objeto global con la URL y el tipo de Parser, en este caso es html y por eso usamos el parser llamado 'html5lib'
-pagina = bs.BeautifulSoup(url, "html5lib")
+######### FUNCTIONS ######### 
 
-# Creamos una variable para almacenar la hora actual
-hora = datetime.datetime.now()
-hora_local = int(hora.hour)-6
-
-
-######### FUNCIONES ######### 
-
-# Creamos una funcion para obtener la temperatura en Celsius llamada GetTemp
 def GetTempC():
-    # Se usa el metodo 'find' para buscar la etiqueta <span> con el atributo 'data-reactid' con valor '37'
-    a = pagina.find("span", {"data-reactid": "37"}).get_text()
-
-    # Se declara la variable c para que se almacene la conversion de Fahrenheit a Celsius
+    a = page.find("span", {"data-reactid": "37"}).get_text()
     c = (int(a)-32)*5/9
 
-    # Lo que se regresa es el redondeo del numero flotante c. 'round()' es para redondear y 'float(c)' es para hacer flotante a 'c'
     return(round(float(c)))
 
-
-# Creamos una funcion para obtener el texto referente al clima (mayormente nublado, despejado, lluvioso, etc.)
 def GetClima():
-    # Se usa el metodo 'find' para buscar la etiqueta <span> con el atributo 'data-reactid' con valor '26'
-    a = pagina.find("span", {"data-reactid": "26"}).get_text()
+    a = page.find("span", {"data-reactid": "26"}).get_text()
 
-    # Se regresa el texto encontrado con el metodo 'find'
     return(str(a))
 
 
@@ -51,8 +34,6 @@ consumer_secret = os.environ['CONSUMER_SECRET']
 access_token = os.environ['ACCESS_TOKEN']
 access_token_secret = os.environ['ACCESS_TOKEN_SECRET']
 
-
-# Se crea un objeto Twython
 twitter = Twython(
     consumer_key,
     consumer_secret,
@@ -60,29 +41,14 @@ twitter = Twython(
     access_token_secret
 )
 
-
 ######### MAIN ######### 
 
 if __name__ == "__main__":
 
-    #Se define 'temp' como la variable que tendra el dato de la temperatura
     temp = GetTempC()
     clima = GetClima()
-
-    # Cambiar 'message' para adaptarlo a tu situacion
-    message = "El clima en #Reynosa #reynosafollow es '"+str(clima)+"' con una temperatura de: '"+str(temp)+"°C' ("+str(hora_local)+":"+str(hora.minute)+"hrs.)"
-
-    # Se manda el tuit con la informacion
+    message = "El clima en #Reynosa #reynosafollow es '"+str(clima)+"' con una temperatura de: '"+str(temp)+"°C' ("+str(local_hour)+":"+str(hour.minute)+"hrs.)"
     twitter.update_status(status=message)
-
-    # Y se imprime en pantalla
     print("Se tuiteo: %s" % message)
 
     pass
-
-
-
-
-
-
-
