e15ea4a1aaf63118adfa6eeb13aa39d1623f2d38
Cesar Pazzi <csrpazzi@gmail.com>
2022-09-17 10:13:57 -0600

    new import, new reactids on yahoo page

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 print("TRYING TO IMPORT LIBRARIES")
 try:
     import bs4 as bs
-    import urllib.request
+    import requests
     from twython import Twython
     from datetime import datetime
     import os
@@ -9,32 +9,33 @@ try:
     print("DONE")
 
 except:
-    print("ERROR! COULD NOT IMPORT LIBRARIES")
+    print("ERROR! CANNOT NOT IMPORT LIBRARIES")
 
 ######### GLOBALS ######### 
 print("SETTING GLOBALS")
 try:
     yahoo_url = os.environ['YAHOO_URL']
-    url = urllib.request.urlopen(yahoo_url).read()
+    url = requests.get(yahoo_url).text
     page = bs.BeautifulSoup(url, "html5lib")
     fecha = datetime.now(pytz.timezone('US/Central'))
     print("DONE")
+
 except:
-    print("ERROR! COULD NOT SET GLOBALS")
+    print("ERROR! CANNOT NOT SET GLOBALS")
 
 print("----------------------------------")
 
 ######### FUNCTIONS ######### 
 
 def GetTempC():
-    a = page.find("span", {"data-reactid": "37"}).get_text()
+    a = page.find("span", {"data-reactid": "42"}).get_text()
     c = (int(a)-32)*5/9
 
     return(round(float(c)))
 
 def GetClima():
-    a = page.find("span", {"data-reactid": "26"}).get_text()
-
+    a = page.find("span", {"data-reactid": "31"}).get_text()
+    print(a)
     return(str(a))
 
 
@@ -45,6 +46,7 @@ consumer_key = os.environ['CONSUMER_KEY']
 consumer_secret = os.environ['CONSUMER_SECRET']
 access_token = os.environ['ACCESS_TOKEN']
 access_token_secret = os.environ['ACCESS_TOKEN_SECRET']
+
 
 twitter = Twython(
     consumer_key,
@@ -59,11 +61,9 @@ if __name__ == "__main__":
     try:
         temp = GetTempC()
         clima = GetClima()
+        message = "El clima en #Reynosa #reynosafollow es '"+str(clima)+"' con una temperatura de: '"+str(temp)+"°C' ("+fecha.strftime("%H:%M")+"hrs.)"
+        twitter.update_status(status=message)
+        print("Se tuiteo: %s" % message)
     except:
         print("ERROR! GetTempC() or GetClima() Failed!")
 
-    message = "El clima en #Reynosa #reynosafollow es '"+str(clima)+"' con una temperatura de: '"+str(temp)+"°C' ("+fecha.strftime("%H:%M")+"hrs.)"
-    twitter.update_status(status=message)
-    print("Se tuiteo: %s" % message)
-
-    pass
