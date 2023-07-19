8647eff521698f9eae6a50b14ab80db0af21ee61
Cesar Pazzi <csrpazzi@gmail.com>
2023-07-19 17:00:56 -0600

    replacing twython for tweepy

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ print("TRYING TO IMPORT LIBRARIES")
 try:
     import bs4 as bs
     import requests
-    from twython import Twython
+    import tweepy
     from datetime import datetime
     import os
     import pytz
@@ -28,13 +28,15 @@ print("----------------------------------")
 ######### FUNCTIONS ######### 
 
 def GetTempC():
-    a = page.find("span", {"data-reactid": "42"}).get_text()
-    c = (int(a)-32)*5/9
-
-    return(round(float(c)))
+    a = page.find("span", {"Va(t) D(n) celsius celsius_D(b)"}).get_text()
+    #Fahrenheit to Celsius
+    #c = (int(a)-32)*5/9
+    #return(round(float(c)))
+    print(a)
+    return(a)
 
 def GetClima():
-    a = page.find("span", {"data-reactid": "31"}).get_text()
+    a = page.find("p", {"Fz(1.40rem)--miw1024 Fz(1.12rem)"}).get_text()
     print(a)
     return(str(a))
 
@@ -42,28 +44,30 @@ def GetClima():
 ######### TWYTHON CONFIG ######### 
 
 # Github Secrets
+
 consumer_key = os.environ['CONSUMER_KEY']
 consumer_secret = os.environ['CONSUMER_SECRET']
 access_token = os.environ['ACCESS_TOKEN']
 access_token_secret = os.environ['ACCESS_TOKEN_SECRET']
 
-
-twitter = Twython(
-    consumer_key,
-    consumer_secret,
-    access_token,
-    access_token_secret
+twitter = tweepy.Client(
+    consumer_key=consumer_key, consumer_secret=consumer_secret,
+    access_token=access_token, access_token_secret=access_token_secret
 )
+
 
 ######### MAIN ######### 
 
 if __name__ == "__main__":
-    try:
+
         temp = GetTempC()
         clima = GetClima()
         message = "El clima en #Reynosa #reynosafollow es '"+str(clima)+"' con una temperatura de: '"+str(temp)+"°C' ("+fecha.strftime("%H:%M")+"hrs.)"
-        twitter.update_status(status=message)
+
+        twitter.create_tweet(text=message)
         print("Se tuiteo: %s" % message)
-    except:
-        print("ERROR! GetTempC() or GetClima() Failed!")
+        print(f"https://twitter.com/user/status/{twitter.data['id']}")
+        # print("ERROR TRYING TO SEND THE TWEET!")
+
+        # print("ERROR! GetTempC() or GetClima() Failed!")
 
